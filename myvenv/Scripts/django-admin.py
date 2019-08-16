3f80acdd6661c77db8fe1c08053c9a2352165539
camilo valencia <camilo3341215@gmail.com>
2019-08-16 09:40:15 -0500

    Los primeros modelos ya se crearon

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\omcam\documents\sena trabajos\trabajos jaramillo\proyectofindbuy\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
